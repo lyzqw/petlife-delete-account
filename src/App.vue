7fda60548ffa5632d9da12b15664588270ed2f99
liuyuzhe <15313482685@163.com>
2025-02-11 13:50:37 +0800

    删除账号完成
    Signed-off-by: liuyuzhe <15313482685@163.com>

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script setup>
+import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+// 删除账号函数
+const deleteAccount = () => {
+  alert('Account deleted successfully!')
+}
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- 删除账号按钮 -->
+  <div class="delete-container">
+    <button class="delete-button" @click="deleteAccount">Delete Account</button>
+  </div>
 </template>
 
 <style scoped>
@@ -25,6 +31,27 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+.delete-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.delete-button {
+  background-color: #ff4d4d;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: background 0.3s;
+}
+
+.delete-button:hover {
+  background-color: #cc0000;
 }
 
 @media (min-width: 1024px) {
